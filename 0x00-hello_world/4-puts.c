c2d371b557e600e0c41d30d95e36a72f95f5c8f2
bontsigotlhe <boipelomolwela@gmail.com>
2023-04-20 19:47:00 -0700

    Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#inlcude<stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multiligual puzzle");
+	return (0)
+}
