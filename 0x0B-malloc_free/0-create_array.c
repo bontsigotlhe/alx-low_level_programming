cbbf168d24225c277fcc9a392e83ee7c598de47a
bontsigotlhe <boipelomolwela@gmail.com>
2023-05-20 13:09:39 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,10 +3,10 @@
 #include "main.h"
 
 /**
-* _strdup - duplicate to new memory space location
-* @str: char
-* Return: 0
-*/
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
 char *_strdup(char *str)
 {
 char *aaa;
