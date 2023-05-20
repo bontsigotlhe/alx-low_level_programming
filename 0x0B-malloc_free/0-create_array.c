36671e4a1cf123f24b9814c7225685d53d73d5fa
bontsigotlhe <boipelomolwela@gmail.com>
2023-05-20 12:10:15 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
 		
  * @c: char to assign
 		
- * Description: creat array of size size and assign char c
+ * Description: create array of size size and assign char c
 		
  * Return: pointer to array, NULL if fail
 		
